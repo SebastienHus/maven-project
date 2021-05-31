02fe22e2f536554ce340b500acb90e6e736ca4c3
Hus Sébastien <hus.sebastien@gmail.com>
2021-05-31 16:19:24 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,10 +2,15 @@ package com.example;
 
 
 public class Greeter {
-  public Greeter() {
-  }
+    public Greeter() {
+    }
 
-  public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
-  }
+    /**
+     * 
+     * @param someone
+     * @return
+     */
+    public final String greet(final String someone) {
+        return String.format("Hello babacar, %s!", someone);
+    }
 }
